412b7b598f56be6e5c30f15d3a30b01ba8951cef
Adarsh Shirawalmath <114558126+adarshxs@users.noreply.github.com>
2023-08-26 09:22:05 +0530

    Update app.py

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -5,20 +5,20 @@ import gradio as gr
 import gc
 from interface.hddr_llama_onnx_interface import LlamaOnnxInterface
 from interface.empty_stub_interface import EmptyStubInterface
-from ChatApp.app_modules.utils import (
+from app_modules.utils import (
     reset_textbox,
     transfer_input,
     reset_state,
     delete_last_conversation,
     cancel_outputing,
 )
-from ChatApp.app_modules.presets import (
+from app_modules.presets import (
     small_and_beautiful_theme,
     title,
     description_top,
     description,
 )
-from ChatApp.app_modules.overwrites import postprocess
+from app_modules.overwrites import postprocess
 
 logging.basicConfig(
     level=logging.DEBUG,
